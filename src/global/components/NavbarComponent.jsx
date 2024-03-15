f62119c8696ebc56ecb7b62081179dc25a6bdb04
Gurjit Samra <104379797+Gurjit-Samra@users.noreply.github.com>
2024-03-15 14:16:46 -0600

    Refactored Navbar component
    to be easier to use, and added hamburger menu mobile responsive feature with media query

--- src/global/components/NavbarComponent.jsx
+++ src/global/components/NavbarComponent.jsx
@@ -1,23 +1,60 @@
 import React from "react";
+import { useState } from "react";
 import '../styles/NavbarComponent.css';
 import { Link } from 'react-router-dom';
 
-const NavbarComponent = () => {
+const HamburgerIcon = () => {
+    return (
+        <div>
+            <span className="menu-line"></span>
+            <span className="menu-line"></span>
+            <span className="menu-line"></span>
+        </div>    
+    );
+}
 
-    return(
-    <section id="desktop-nav">
-        <div className="nav-bar-container">
-            <div className="nav-bar-logo">
-                <Link to="/Home"><img src="/assets/GS.png" alt="Gurjit Samra Logo" /></Link>
-            </div>
-            <div className="nav-bar-links-container">
-                <li><Link className="nav-bar-links" to="/About">About</Link></li>
-                <li><Link className="nav-bar-links" to="/Projects">Projects</Link></li>
-                <li><Link className="nav-bar-links" to="/Contact">Contact</Link></li>
-            </div>
+const NavItem = (props) => {
+    return (
+        <div className={props.className}>
+        {props.children}
         </div>
-    </section>
     );
 };
 
+const NavbarComponent = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return(
+
+    <nav className="nav-bar-container">
+
+        <NavItem className="nav-bar-logo">
+            <Link to="/Home"><img src="/assets/GS.png" alt="Gurjit Samra Logo" /></Link>
+        </NavItem>
+        
+        <NavItem className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onclick={toggleMenu}>
+            <span className="menu-line"></span>
+            <span className="menu-line"></span>
+            <span className="menu-line"></span>
+        </NavItem>
+
+        <NavItem className="nav-bar-links-container">
+            <li><Link className="nav-bar-links" to="/About">About</Link></li>
+            <li><Link className="nav-bar-links" to="/Projects">Projects</Link></li>
+            <li><Link className="nav-bar-links" to="/Contact">Contact</Link></li>
+        </NavItem>
+
+    </nav>
+        
+    
+    );
+};
+
+
+export { HamburgerIcon, NavItem };
 export default NavbarComponent;
