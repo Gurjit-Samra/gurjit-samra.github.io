56623e161fa4fa6a8893b7b2e4ddafd4a1c11ca8
Gurjit Samra <104379797+Gurjit-Samra@users.noreply.github.com>
2024-04-15 22:16:34 -0600

    changed href attribute

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
         <div className="hero-container">
           <div id="hero-text-and-button-container">
             <h1 className="titles">Gurjit Samra</h1>
-            <a href="/Contact"><button className="contact-button">Get in touch</button></a>
+            <a to="/Contact"><button className="contact-button">Get in touch</button></a>
           </div>
         </div>
 
